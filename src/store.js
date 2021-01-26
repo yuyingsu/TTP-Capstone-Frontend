a0245ecffecc9d07c597f05436a4b4f7b83a8da9
Scarcrux <reg@scarcrux.com>
2021-01-26 18:44:46 -0500

    Post Shipping Form Without Validation - #17

--- src/store.js
+++ src/store.js
@@ -6,6 +6,15 @@ import {
   userRegisterReducer,
   userUpdateReducer,
 } from './reducers/userReducers';
+import Cookie from 'js-cookie';
+
+const cartItems = Cookie.getJSON('cartItems') || [];
+const userInfo = Cookie.getJSON('userInfo') || null;
+
+const initialState = {
+  cart: { cartItems, shipping: {}, payment: {} },
+  userSignin: { userInfo },
+};
 
 const rootReducer = combineReducers({
     pds: productReducers,
@@ -17,6 +26,7 @@ const storeEnhancers = window.REDUX_DEVTOOLS_EXTENSION_COMPOSE || compose;
 
 const store = createStore(
   rootReducer,
+  initialState,
   storeEnhancers(applyMiddleware(thunk))
 );
 export default store;
