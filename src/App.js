56e7b91579ecc3cc5c601f0be298c5283bf44da0
Scarcrux <reg@scarcrux.com>
2021-01-26 12:01:57 -0500

    Create Search Box - #38

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import { Product, Register, Signin } from './screens/';
+import { Home, Register, Signin } from './screens/';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Route exact path="/" component={Product} />
+        <Route exact path="/" component={Home} />
         <Route exact path="/register" component={Register} />
         <Route exact path="/signin" component={Signin} />
       </BrowserRouter>
--- src/screens/index.js
+++ src/screens/index.js
@@ -1,3 +1,3 @@
-export { default as Product } from './Products'
+export { default as Home } from './Home'
 export { default as Register } from './Register'
 export { default as Signin } from './Signin'
