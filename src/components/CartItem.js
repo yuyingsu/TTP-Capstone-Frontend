12091b848b84adbf010001d8e2db7b9c282d1b9c
Scarcrux <reg@scarcrux.com>
2021-01-29 10:00:05 -0500

    Adjust Cart Styling - #12

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,27 +1,51 @@
 import React from 'react';
 import { deleteFromCart, changeQtyInCart } from '../actions/cartActions';
 import { useDispatch } from 'react-redux';
+import { Container, Row, Col } from 'reactstrap';
+
 function CardProduct(props) {
   const dispatch = useDispatch();
   const deleteProduct = () =>{
-      dispatch(deleteFromCart(props.cart._id));
+    dispatch(deleteFromCart(props.cart._id));
   }
   const changeQty = (qty) => {
-      dispatch(changeQtyInCart(props.cart._id,qty));
+    dispatch(changeQtyInCart(props.cart._id, parseInt(qty)));
   }
-  let price = props.cart.price*props.cart.qty;
+  let price = props.cart.price * props.cart.qty;
+
   return(
-        <div>
-            {props.cart.name}
-            <img src={props.cart.image} style={{height:"50px", width:"50px"}}/>
-            <div>{price}</div>
-            <select onChange={(e)=>{changeQty(e.target.value)}}>
-            {[...Array(props.cart.countInStock).keys()].map(x =>
-                        <option key={x + 1} value={x + 1}>{x + 1}</option>
-                      )}
+    <div class="item">
+
+    <div class="image">
+        <img src={props.cart.image} style={{height:"100px", width:"100px"}}/>
+    </div>
+
+    <div class="description">
+        <h5>{props.cart.name}</h5>
+    </div>
+
+    <div class="quantity">
+      <span><select id="item-quantity" onChange={(e)=>{changeQty(e.target.value)}}>
+            {[...Array(props.cart.countInStock).keys()].slice(0,10).map(x => {
+                if (x + 1 === props.cart.qty) {
+                    return <option selected="selected">{props.cart.qty}</option>;
+                } else {
+                    return <option key={x + 1} value={x + 1}>{x + 1}</option>;
+                }
+              }
+            )}
+
             </select>
-            <button onClick={deleteProduct}>Delete</button>
-        </div>
+            </span>
+    </div>
+
+    <div class="total-price ml-auto"><span><h5>{"$" + price}</h5></span>
+    <span><a href="#" onClick={deleteProduct}>Remove</a></span>
+    </div>
+  </div>
+
+
+
     );
 }
 
