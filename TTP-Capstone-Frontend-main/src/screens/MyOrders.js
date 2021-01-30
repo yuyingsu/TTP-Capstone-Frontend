27760090760203df0ba8a56650c2f770cf4b0c0f
yuyingsu <yuyingsujob@gmail.com>
2021-01-30 10:53:57 -0500

    Enhance pagination and user registration

---
+++ TTP-Capstone-Frontend-main/src/screens/MyOrders.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { logout, update } from '../actions/userActions';
+import { listMyOrders } from '../actions/orderActions';
+import { useDispatch, useSelector } from 'react-redux';
+
+function MyOrders(props) {
+  const dispatch = useDispatch();
+  const myOrderList = useSelector(state => state.myOrderList);
+  const { loading: loadingOrders, orders, error: errorOrders } = myOrderList;
+  useEffect(() => {
+    dispatch(listMyOrders());
+    return () => {
+
+    };
+  }, [])
+
+  return <div className="profile-orders content-margined">
+{
+  loadingOrders ? <div>Loading...</div> :
+    errorOrders ? <div>{errorOrders} </div> :
+      <table className="table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>DATE</th>
+            <th>TOTAL</th>
+            <th>PAID</th>
+            <th>ACTIONS</th>
+          </tr>
+        </thead>
+        <tbody>
+          {orders.map(order => <tr key={order._id}>
+            <td>{order._id}</td>
+            <td>{order.createdAt}</td>
+            <td>{"$" + order.totalPrice.toFixed(2)}</td>
+            <td>{order.isPaid}</td>
+            <td>
+              <Link to={"/order/" + order._id}>DETAILS</Link>
+            </td>
+          </tr>)}
+        </tbody>
+      </table>
+}
+</div>
+}
+
+export default MyOrders;
